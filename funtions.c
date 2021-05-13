98d83deef21263dd63beb516a5eebd5cd3aa1454
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-13 21:58:19 +0000

    [ADD] files

---
+++ funtions.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+/**
+ * fn_push - add new node in the stack.
+ * @stack: structure
+ * @line_number: line where is the function.
+ * @n: integer.
+ *
+ * Return: None.
+ */
+void fn_push(stack_t **stack, unsigned int line_number, char *n)
+{
+	stack_t *new_node = NULL;
+
+	if (!n)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = atoi(n);
+	new_node->next = NULL;
+	if (*stack != NULL)
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+}
+/**
+* fn_pall - prints all the values on the stack, starting from the top.
+* @stack: structure
+* @line_number: line where is the function.
+*
+* Return: None.
+*/
+void fn_pall(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+	stack_t *temp = *stack;
+
+	(void)line_number;
+	if (!temp)
+		return;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+}
+/**
+* fn_pint -  prints the value at the top of the stack, followed by a new line.
+* @stack: structure
+* @line_number: line where is the function.
+*
+* Return: None.
+*/
+
+void fn_pint(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("%d\n", (*stack)->n);
+}
---
+++ main.c
@@ -0,0 +1,72 @@
+#include "monty.h"
+#define _LIMITS_ " \t\n"
+/**
+ * main - principal function.
+ * @ac: number of parameters.
+ * @av: parameter.
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	stack_t *stack = NULL;
+	char *line = NULL, *token, *ch;
+	size_t len = 0;
+	FILE *files;
+	int num = 0;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	files = fopen(av[1], "r");
+	if (!files)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, files) != EOF)
+	{
+		token = strtok(line, _LIMITS_);
+		num++;
+		if (strcmp(token, "push") == 0)
+		{
+			ch = strtok(NULL, _LIMITS_);
+			fn_push(&stack, num, ch);
+		}
+		else
+		{
+			select_fn(&stack, token, num);
+		}
+	}
+	fclose(files);
+	return (0);
+}
+/**
+ * select_fn - select the necessary function.
+ * @stack: structure.
+ * @token: function to select.
+ * @line_number: line where is the function.
+ *
+ * Return: None
+ */
+void select_fn(stack_t **stack, char *token, unsigned int line_number)
+{
+	int i;
+
+	instruction_t fn_op[] = {
+		{"pall", fn_pall},
+		{"pint", fn_pint},
+		{NULL, NULL}};
+	for (i = 0; fn_op[i].opcode != NULL; i++)
+	{
+		if (strcmp(token, fn_op[i].opcode) == 0)
+		{
+			(fn_op[i].f)(stack, line_number);
+			return;
+		}
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
+	exit(EXIT_FAILURE);
+}
