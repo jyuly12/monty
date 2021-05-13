08cd9363e97da79b407ec8ad07a04e59a717e549
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-13 22:00:27 +0000

    [ADD] libraries

---
+++ monty.h
@@ -0,0 +1,46 @@
+#ifndef FILES_H
+#define FILES_H
+
+/*libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/*functions*/
+void select_fn(stack_t **stack, char *token, unsigned int line_number);
+void fn_push(stack_t **stack, unsigned int line_number, char *n);
+void fn_pint(stack_t **stack, unsigned int line_number);
+void fn_pall(stack_t **stack, unsigned int line_number);
+
+#endif
